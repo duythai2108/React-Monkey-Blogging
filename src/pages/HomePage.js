12edbf952454a84f0ea8d7dd9a778a85c3c9502b
duythai2108 <thaiduy21082000@gmail.com>
2023-07-26 09:51:12 +0700

    update hompage UI and styles

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,6 +3,8 @@ import Layout from "components/layout/Layout";
 import { auth } from "firebase-app/firebase-config";
 import { signOut } from "firebase/auth";
 import HomeBanner from "module/home/HomeBanner";
+import HomeFeature from "module/home/HomeFeature";
+import HomeNewest from "module/home/HomeNewest";
 import React from "react";
 import styled from "styled-components";
 
@@ -12,6 +14,8 @@ const HomePage = () => {
     <HomePageStyles>
       <Layout>
         <HomeBanner></HomeBanner>
+        <HomeFeature></HomeFeature>
+        <HomeNewest></HomeNewest>
       </Layout>
     </HomePageStyles>
   );
