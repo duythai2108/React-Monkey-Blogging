f4328f26283c14bcfb40ac0f2aaf42dec93e2aad
duythai2108 <thaiduy21082000@gmail.com>
2023-07-28 10:06:33 +0700

    manage category: Load + Delete Category SweetAlert

--- src/module/category/CategoryManage.js
+++ src/module/category/CategoryManage.js
@@ -1,13 +1,96 @@
+import { ActionDelete, ActionEdit, ActionView } from "components/action";
+import { LabelStatus } from "components/label";
+import { Table } from "components/table";
+import { db } from "firebase-app/firebase-config";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  getDoc,
+  onSnapshot,
+} from "firebase/firestore";
 import DashboardHeading from "module/dashboard/DashboardHeading";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { categoryStatus } from "utils/constants";
+import Swal from "sweetalert2";
 
 const CategoryManage = () => {
+  const [categoryList, setCategoryList] = useState([]);
+  useEffect(() => {
+    const colRef = collection(db, "categories");
+    onSnapshot(colRef, (snapshot) => {
+      let results = [];
+      snapshot.forEach((doc) => {
+        results.push({
+          id: doc.id,
+          ...doc.data(),
+        });
+      });
+      setCategoryList(results);
+    });
+  }, []);
+  const handleDeleteCategory = async (docId) => {
+    const colRef = doc(db, "categories", docId);
+
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        await deleteDoc(colRef);
+        Swal.fire("Deleted!", "Your file has been deleted.", "success");
+      }
+    });
+  };
   return (
     <div>
       <DashboardHeading
         title="Categories"
         desc="Manage your category"
       ></DashboardHeading>
+      <Table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Slug</th>
+            <th>Status</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {categoryList.length > 0 &&
+            categoryList.map((category) => (
+              <tr key={category.id}>
+                <td>{category.id}</td>
+                <td>{category.name} </td>
+                <td className="italic text-gray-500">{category.slug}</td>
+                <td>
+                  {category.status === categoryStatus.APPROVED && (
+                    <LabelStatus type="success">Approved</LabelStatus>
+                  )}
+                  {category.status === categoryStatus.UNAPPROVED && (
+                    <LabelStatus type="warning">Approved</LabelStatus>
+                  )}
+                </td>
+                <td>
+                  <div className="flex items-center gap-x-3">
+                    <ActionView></ActionView>
+                    <ActionEdit></ActionEdit>
+                    <ActionDelete
+                      onClick={() => handleDeleteCategory(category.id)}
+                    ></ActionDelete>
+                  </div>
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </Table>
     </div>
   );
 };
