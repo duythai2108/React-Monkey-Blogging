bcafcb1a5107567ca92f23c7ab14670ac64235b5
duythai2108 <thaiduy21082000@gmail.com>
2023-08-03 14:30:33 +0700

    add lazy load suspense all page + proptype for input

--- src/components/input/Input.js
+++ src/components/input/Input.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { useController } from "react-hook-form";
+import PropTypes from "prop-types";
 
 const InputStyles = styled.div`
   position: relative;
@@ -51,5 +52,10 @@ const Input = ({ name = "", type = "text", children, control, ...props }) => {
     </InputStyles>
   );
 };
-
+Input.propTypes = {
+  name: PropTypes.string.isRequired,
+  type: PropTypes.string,
+  children: PropTypes.any,
+  control: PropTypes.any.isRequired,
+};
 export default Input;
